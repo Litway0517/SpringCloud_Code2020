3f92cbc0bdbf3706ea063b60f185ba317d270ea1
Litway_ <xmw0517@gmail.com>
2022-09-10 15:18:50 +0800

    order  模拟用户的微服务

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 订单main80
+ *
+ * @author DELL_
+ * @date 2022/09/10
+ */
+@SpringBootApplication
+public class OrderMain80 {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(OrderMain80.class,args);
+    }
+}
