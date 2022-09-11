74f3ac791e497b75545f84c98073acd7b5a1c7e2
Litway_ <xmw0517@gmail.com>
2022-09-11 16:11:07 +0800

    payment  在需要注入Eureka Server服务端的微服务模块中导入spring-cloud-starter-netflix-eureka-client坐标 然后配置文件中配置相关的eureka初始化值 最后启动类上加上@EnableEurekaClient注解

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 /**
@@ -11,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2022/09/09
  */
 @SpringBootApplication
+// 开启Eureka Client客户端, 使微服务进入到Eureka Server服务器
+@EnableEurekaClient
 public class PaymentMain8001 {
 
     public static void main(String[] args) {
