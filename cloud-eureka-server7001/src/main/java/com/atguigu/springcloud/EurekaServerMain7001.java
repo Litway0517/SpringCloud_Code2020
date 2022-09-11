62702508fda1dfce4761af1c40143413d3ab855c
Litway_ <xmw0517@gmail.com>
2022-09-11 11:11:55 +0800

    eureka  Eureka有两个组件分别是Eureka Server和Eureka Client 因为eureka设计就是CS模式 所以在Eureka Server的主启动类上需要加上@EnableEurekaServer的注解 同样的在其他的client实例的启动类上需要加上@EnableEurekaClient注解

---
+++ cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaServerMain7001.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 尤里卡服务器main7001
+ *
+ * @author DELL_
+ * @date 2022/09/11
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerMain7001.class, args);
+    }
+}
