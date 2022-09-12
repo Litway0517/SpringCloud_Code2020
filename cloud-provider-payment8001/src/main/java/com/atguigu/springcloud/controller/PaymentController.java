5c34210e20e83688d6a7bc32a22ae55e0572f417
Litway_ <xmw0517@gmail.com>
2022-09-12 19:49:35 +0800

    payment集群  主启动类上加上@EnableDiscoveryClient注解(好像不加也行) 在8001中注入DiscoveryClient这个类能够获得响应的Eureka集群中存在的微服务名称 然后就能够通过服务名称获取具体微服务下面的所有实例 进而达到获取所有ip port

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 付款控制器
@@ -21,6 +24,13 @@ public class PaymentController {
 
     @Resource
     private PaymentService paymentService;
+
+    /*
+        @DiscoveryService服务发现 的注解. 能够得到Eureka Server中注册的一些信息.
+        例如, 有哪些微服务, 具体对应的微服务下面又有多少实例, 具体实例中的URI, port等信息.
+     */
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @Value("${server.port}")
     private String serverPost;
@@ -43,6 +53,30 @@ public class PaymentController {
         } else {
             return new CommonResult(444, "插入数据库失败", null);
         }
+    }
+
+
+    /*
+        测试DiscoveryClient这个类中有什么信息
+     */
+    @GetMapping(name = "test", value = "/payment/discoveryClient")
+    public CommonResult<DiscoveryClient> discoveryClient() {
+        List<String> services = discoveryClient.getServices();
+        services.forEach(s -> {
+            log.info("Eureka Server中的微服务名称：" + s);
+        });
+
+        // 直接获得cloud-payment-service对应的实例有多少
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        instances.forEach(s -> {
+            log.info("ServiceId: " + s.getServiceId());
+            log.info("Host: " + s.getHost());
+            log.info("Port: " + s.getPort());
+            log.info("URL: " + s.getUri());
+            log.info("Scheme: " + s.getScheme());
+            log.info("\n");
+        });
+        return new CommonResult<DiscoveryClient>(200, "DiscoveryClient相关信息", discoveryClient);
     }
 
 }
