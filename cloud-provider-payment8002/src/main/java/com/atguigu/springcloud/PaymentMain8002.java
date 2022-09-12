df6f8e4002ee9dc9c04be48a3747814336fd08e0
Litway_ <xmw0517@gmail.com>
2022-09-12 11:16:38 +0800

    payment集群  8002更改主类 另外pom文件的artifactId重复了导致启动失败

--- cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/PaymentMain8002.java
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/PaymentMain8002.java
@@ -4,19 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
-/**
- * 付款main8002
- *
- * @author DELL_
- * @date 2022/09/12
- */
 @SpringBootApplication
-// 开启Eureka Client客户端, 使微服务进入到Eureka Server服务器
 @EnableEurekaClient
 public class PaymentMain8002 {
 
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8002.class, args);
     }
+
 }
