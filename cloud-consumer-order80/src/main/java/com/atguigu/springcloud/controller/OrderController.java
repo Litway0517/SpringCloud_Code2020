d5a121c59c03096bf5c1c37f9322b814bff0cad1
Litway_ <xmw0517@gmail.com>
2022-09-12 11:58:32 +0800

    order  payment微服务集群对外暴露的不再是ip:port而是微服务的名称

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -11,7 +11,16 @@ import javax.annotation.Resource;
 @RequestMapping("/consumer")
 public class OrderController {
 
-    private static final String PAYMENT_URL = "http://localhost:8001/payment";
+    // 以前是单个payment微服务, 但是更改为CLOUD-PAYMENT-SERVICE
+    // private static final String PAYMENT_URL = "http://localhost:8001/payment";
+
+    /*
+        现在 支付payment微服务 内部有两台微服务运行, 向外暴露出来的微服务名称就不能是 8001 8002标志了,
+        那样的话让客户端调用就会出现让客户端选择调用哪台微服务, 显然不可能.
+        因此, 我们使用微服务的名称, 他们的名称是一样的, 这样能够说明. 这样 支付payment微服务集群, 对外暴露的就不再是ip:port而是服务名.
+     */
+    private static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
+
 
     @Resource
     private RestTemplate restTemplate;
