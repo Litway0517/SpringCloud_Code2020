a40afd0c20015be79686ad575b9fe7aa3652a5eb
Litway_ <xmw0517@gmail.com>
2022-09-12 12:04:57 +0800

    order  调用的接口改成了payment集群的服务名称 别忘了加上consumer

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -19,7 +19,7 @@ public class OrderController {
         那样的话让客户端调用就会出现让客户端选择调用哪台微服务, 显然不可能.
         因此, 我们使用微服务的名称, 他们的名称是一样的, 这样能够说明. 这样 支付payment微服务集群, 对外暴露的就不再是ip:port而是服务名.
      */
-    private static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
+    private static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE/consumer";
 
 
     @Resource
