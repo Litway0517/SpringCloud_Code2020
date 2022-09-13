baa12a5286476f5d5d235ac2ecfabd86eec12bd9
Litway_ <xmw0517@gmail.com>
2022-09-13 10:57:14 +0800

    payment集群  8004支付微服务 配置文件和相关启动类 启动类上要加上@EnableDiscoveryClient注解

---
+++ cloud-provider-payment8004/src/test/java/com/atguigu/springcloud/PaymentMain8004Tests.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentMain8004Tests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
