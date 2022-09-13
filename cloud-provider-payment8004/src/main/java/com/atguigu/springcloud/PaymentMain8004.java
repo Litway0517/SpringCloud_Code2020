3616433204161901c99b27c5a4178df98c9f1f18
Litway_ <xmw0517@gmail.com>
2022-09-13 15:15:14 +0800

    payment集群  8004支付微服务 因为zookeeper依赖中也含有slf4j所以需要统统排除 不然启动就会报错 slf4j默认只能单独运行

--- cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/PaymentMain8004.java
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/PaymentMain8004.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 付款main8004
+ *
+ * @author DELL_
+ * @date 2022/09/13
+ */
 @SpringBootApplication
 // 该注解用于向使用consul或者zookeeper作为注册中心时注册服务
 @EnableDiscoveryClient
