b8b328081bcbd73a6ac5bdaebfc14af8a9482845
Litway_ <xmw0517@gmail.com>
2022-09-15 20:27:02 +0800

    payment  相关controller层接口

--- cloud-providerconsul-payment8006/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-providerconsul-payment8006/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -1,12 +1,22 @@
 package com.atguigu.springcloud.controller;
 
 import cn.hutool.core.lang.UUID;
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 public class PaymentController {
+
+    @Resource
+    private PaymentService paymentService;
 
     @Value("${server.port}")
     private String serverPort;
@@ -14,6 +24,16 @@ public class PaymentController {
     @RequestMapping(value = "/payment/consul")
     public String paymentConsul() {
         return "springcloud with consul: " + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
+        Payment payment = paymentService.getPaymentById(id);
+        if (payment != null) {
+            return new CommonResult(200, "查询成功, server-port = " + serverPort, payment);
+        } else {
+            return new CommonResult(444, "没有对应记录,查询ID: " + id, null);
+        }
     }
 
 }
