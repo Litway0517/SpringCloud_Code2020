cccb8cbf333d7d10b4984caffd269d5a078af4f8
Litway_ <xmw0517@gmail.com>
2022-09-15 20:25:53 +0800

    consumer|provider  引入通用依赖 调用8006微服务

--- cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/controller/OrderConsulController.java
+++ cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/controller/OrderConsulController.java
@@ -1,6 +1,10 @@
 package com.atguigu.springcloud.controller;
 
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,9 +18,22 @@ public class OrderConsulController {
 
     private static final String PAYMENT_URL = "http://CONSUL-PROVIDER-PAYMENT/payment";
 
+    private static final String PAYMENT_SERVICE_URL = "http://CLOUD-PAYMENT-SERVICE/payment";
+
     @GetMapping(value = "/consumer/payment/consul")
     public String consumerConsul() {
         return restTemplate.getForObject(PAYMENT_URL + "/consul", String.class);
+    }
+
+    @GetMapping(value = "/consumer/payment/getEntity/consul/{id}")
+    public CommonResult<Payment> consumerConsulForEntity(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> response = restTemplate.getForEntity(PAYMENT_SERVICE_URL + "/get/" + id, CommonResult.class);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            return response.getBody();
+        } else {
+            return new CommonResult<>(444, "操作失败~");
+        }
+
     }
 
 }
