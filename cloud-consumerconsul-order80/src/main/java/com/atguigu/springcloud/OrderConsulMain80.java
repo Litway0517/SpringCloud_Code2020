797bb9e8295823cfa8cdb23d130c1c170fd13159
Litway_ <xmw0517@gmail.com>
2022-09-16 16:03:50 +0800

    order  主启动类上需要加上@RibbonClient注解 同时这个注解的value值需要和controller层的服务地址保持一样的大小写 要么都是大写要么都是小写

--- cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/OrderConsulMain80.java
+++ cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/OrderConsulMain80.java
@@ -1,11 +1,21 @@
 package com.atguigu.springcloud;
 
+import com.atguigu.myrule.config.RibbonRandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+
+/*
+    在启动该微服务的时候就能去加载我们的自定义Ribbon配置类, 从而使配置生效.
+
+    官方文档明确给出了警告: 这个自定义配置类不能放在@ComponentScan所扫描的当前包下以及子包下,
+        否则我们自定义的这个配置类就会被所有的Ribbon客户端所共享, 达不到特殊化定制的目的了.
+ */
+@RibbonClient(value = "CONSUL-PROVIDER-PAYMENT", configuration = {RibbonRandomRule.class})
 public class OrderConsulMain80 {
 
     public static void main(String[] args) {
