cd9a6ffe968b82daedf6992369971a7a0726efee
Litway_ <xmw0517@gmail.com>
2022-09-23 15:33:14 +0800

    gateway  AfterPredicate是指 规定请求的时间必须在xxx之后 但是xxx需要有格式要求 通过ZoneDateTime.now()实例得到

---
+++ cloud-gateway-gateway9527/src/test/java/com/atguigu/springcloud/GatewayMain9527Test.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud;
+
+
+import java.time.ZonedDateTime;
+
+public class GatewayMain9527Test {
+
+    public static void main(String[] args) {
+
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+
+    }
+
+}
