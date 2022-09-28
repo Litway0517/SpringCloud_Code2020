fbdbcfc6b1da3daeffdeb9eeefdb3feee59546b9
Litway_ <xmw0517@gmail.com>
2022-09-28 19:39:32 +0800

    config-client  配置文件中添加actuator暴露的接口 在controller层上添加@RefreshScope注解 刷新配置

--- cloud-config-client3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
+++ cloud-config-client3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -2,18 +2,20 @@ package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.CommonResult;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${config.info}")
     private String configInfo;
 
     @GetMapping("/get/config-info")
-    public CommonResult getConfigInfo() {
-        return new CommonResult(200, "获取成功", configInfo);
+    public CommonResult<String> getConfigInfo() {
+        return new CommonResult<>(200, "获取成功", configInfo);
     }
 
 }
