197b3e9289f0569ab951156907682e140a6b8a84
Litway_ <xmw0517@gmail.com>
2022-09-29 23:49:37 +0800

    config-client  更改微服务名称 加上@RefreshScope注解 路径是include不是exclude注意一下

--- cloud-config-client3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
+++ cloud-config-client3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -2,10 +2,14 @@ package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.CommonResult;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
+// 加上刷新注解的配置
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${server.port}")
