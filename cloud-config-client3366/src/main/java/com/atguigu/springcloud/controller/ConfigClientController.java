bb43331a4f2b12d58e2fee3637b4123540b164ab
Litway_ <xmw0517@gmail.com>
2022-09-29 16:17:02 +0800

    config-client  3355和3366区分开

--- cloud-config-client3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
+++ cloud-config-client3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -2,12 +2,10 @@ package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.CommonResult;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RefreshScope
 public class ConfigClientController {
 
     @Value("${serve.port}")
