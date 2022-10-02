36fcb63349371d82e8f541ce7bd8e433624968ae
Litway_ <xmw0517@gmail.com>
2022-10-02 22:28:20 +0800

    nacos  微服务集群controller层

---
+++ cloud-alibaba-provider-payment9002/src/main/java/com/atguigu/springcloud/controller/PaymentNacosController.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class PaymentNacosController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+
+    @GetMapping("/payment/nacos/{id}")
+    public CommonResult<String> getPayment(@PathVariable("id") Integer id) {
+        String s = "nacos registry, serverPort: " + serverPort + ".\tid" + id;
+        return new CommonResult<>(200, "请求成功", s);
+    }
+
+
+
+}
