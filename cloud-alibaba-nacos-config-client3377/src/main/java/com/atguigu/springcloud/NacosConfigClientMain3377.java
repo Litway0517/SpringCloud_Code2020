f286e061458fe2dcaafee343ee0e6fc6403077c0
Litway_ <xmw0517@gmail.com>
2022-10-08 21:23:06 +0800

    nacos-client

--- cloud-alibaba-nacos-config-client3377/src/main/java/com/atguigu/springcloud/NacosConfigClientMain3377.java
+++ cloud-alibaba-nacos-config-client3377/src/main/java/com/atguigu/springcloud/NacosConfigClientMain3377.java
@@ -5,6 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 纳科配置客户端main3377
+ *
+ * @author DELL_
+ * @date 2022/10/08
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class NacosConfigClientMain3377 {
