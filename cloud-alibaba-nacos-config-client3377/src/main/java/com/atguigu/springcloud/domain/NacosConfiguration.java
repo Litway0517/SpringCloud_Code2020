31465d194d60ecf8da400a9b5caebc3947b00ba3
Litway_ <xmw0517@gmail.com>
2022-10-11 21:19:54 +0800

    nacos-client  测试配置类

---
+++ cloud-alibaba-nacos-config-client3377/src/main/java/com/atguigu/springcloud/domain/NacosConfiguration.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud.domain;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration("config")
+public class NacosConfiguration {
+
+    @Value("${info}")
+    private String info;
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
