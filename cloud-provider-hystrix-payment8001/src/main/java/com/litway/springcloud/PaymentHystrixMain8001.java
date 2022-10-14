ff3d527728f2287adb97d8cf1abccd322695d668
Litway_ <xmw0517@gmail.com>
2022-10-14 14:05:37 +0800

    hystrix  主启动类上加上@EnableCircuitBreaker注解启动Hystrix功能

--- cloud-provider-hystrix-payment8001/src/main/java/com/litway/springcloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/litway/springcloud/PaymentHystrixMain8001.java
@@ -3,10 +3,14 @@ package com.litway.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+
+// 启用Hystrix
+@EnableCircuitBreaker
 public class PaymentHystrixMain8001 {
 
     public static void main(String[] args) {
--- cloud-provider-hystrix-payment8001/src/main/java/com/litway/springcloud/service/PaymentHystrixService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/litway/springcloud/service/PaymentHystrixService.java
@@ -1,6 +1,8 @@
 package com.litway.springcloud.service;
 
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -18,13 +20,27 @@ public class PaymentHystrixService {
     /**
      * 超时访问，演示降级
      */
+    /*
+        fallbackMethod参数 -> 当出现异常情况时, 如调用超时|发生异常, 指定兜底的方法
+        commandProperties参数 -> 设置方法正常运行的峰值参数为3s, 超过3s则执行fallbackMethod指定的兜底方法
+     */
+    @HystrixCommand(fallbackMethod = "reqTimeoutHandler", commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="3000")
+    })
     public String reqTimeout(Integer id) {
-        try {
-            TimeUnit.SECONDS.sleep(3);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return "线程池:" + Thread.currentThread().getName() + "paymentInfo_TimeOut,id: " + id + "\t" + "O(∩_∩)O，耗费3秒";
+
+        int i = 10 / 0;
+
+        // try {
+        //     TimeUnit.SECONDS.sleep(5);
+        // } catch (InterruptedException e) {
+        //     e.printStackTrace();
+        // }
+        return "线程池:" + Thread.currentThread().getName() + "  paymentInfo_TimeOut,id: " + id + "\t" + "O(∩_∩)O";
+    }
+
+    public String reqTimeoutHandler(Integer id) {
+        return "线程池:" + Thread.currentThread().getName() + "系统繁忙，请稍后重试。id：" + id;
     }
 }
 
