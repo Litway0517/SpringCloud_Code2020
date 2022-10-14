5eebfe49ced97c0d2082d10efa5b622936250431
Litway_ <xmw0517@gmail.com>
2022-10-14 16:39:49 +0800

    hystrix  正常调用

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
@@ -3,6 +3,8 @@ package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,9 +24,19 @@ public class OrderHystrixController {
         return paymentService.reqOK(id);
     }
 
+    @HystrixCommand(fallbackMethod = "testTimeoutApiHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
+    })
     @GetMapping("/payment/timeout/{id}")
     public CommonResult<String> testTimeoutApi(@PathVariable("id") Integer id) {
-        return paymentService.reqTimeout(id);
+        CommonResult<String> result = paymentService.reqTimeout(id);
+        return result;
     }
+
+    public CommonResult<String> testTimeoutApiHandler(@PathVariable("id") Integer id) {
+        return new CommonResult<>(200, "请求失败", "消费端80调用支付微服务8001失败，系统繁忙请稍后重试");
+    }
+
+
 
 }
