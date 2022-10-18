081591036b0b117b90d5337ab00f0d7d9f24409b
Litway_ <xmw0517@gmail.com>
2022-10-18 21:46:25 +0800

    sentinel  SentinelResource的blockHandlerClass用来指定兜底类 blockHandler用来指定兜底方法

--- cloud-alibaba-sentinel-service8500/src/main/java/com/atguigu/springcloud/controller/RateLimitController.java
+++ cloud-alibaba-sentinel-service8500/src/main/java/com/atguigu/springcloud/controller/RateLimitController.java
@@ -1,11 +1,14 @@
 package com.atguigu.springcloud.controller;
 
 
+import cn.hutool.core.util.IdUtil;
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.myhandler.CostumerHandlerException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/rate")
 public class RateLimitController {
-
 
     @GetMapping("/test/source")
     @SentinelResource(value = "source", blockHandler = "sourceHandler")
@@ -30,6 +32,30 @@ public class RateLimitController {
     public CommonResult<?> byUrl() {
         return new CommonResult<>().success("操作成功");
     }
+
+
+    // 测试自定义的处理方法
+    @GetMapping("/test/costumerAndParam/{id}")
+    @SentinelResource(value = "costumerAndParam",
+            // 被流控时执行兜底的类
+            blockHandlerClass = CostumerHandlerException.class,
+            // 指定类中的兜底方法
+            blockHandler = "costumerAndParamHandler"
+    )
+    public CommonResult<?> costumerAndParam(@PathVariable("id") Long userId) {
+        return new CommonResult<>().success(new Payment(20L, "有参数: " + IdUtil.fastSimpleUUID() + userId.toString()));
+    }
+
+    @GetMapping("/test/costumerNoParam")
+    @SentinelResource(value = "costumerNoParam",
+            blockHandlerClass = CostumerHandlerException.class,
+            blockHandler = "testCostumerNoParamHandler"
+    )
+    public CommonResult<?> testCostumerNoParam() {
+        return new CommonResult<>().success(new Payment(20L, "无参数: " + IdUtil.fastSimpleUUID()));
+    }
+
+
 
 
 }
