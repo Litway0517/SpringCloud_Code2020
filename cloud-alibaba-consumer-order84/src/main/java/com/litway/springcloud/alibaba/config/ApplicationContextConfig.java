1549dd5f51c4783ea00c71cbfcec66bb0b0f120c
Litway_ <xmw0517@gmail.com>
2022-10-19 09:36:09 +0800

    sentinel-ribbon  修改一些配置

---
+++ cloud-alibaba-consumer-order84/src/main/java/com/litway/springcloud/alibaba/config/ApplicationContextConfig.java
@@ -0,0 +1,19 @@
+package com.litway.springcloud.alibaba.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+
+// RestTemplate是一个配置, 不是组件
+@Configuration
+public class ApplicationContextConfig {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
