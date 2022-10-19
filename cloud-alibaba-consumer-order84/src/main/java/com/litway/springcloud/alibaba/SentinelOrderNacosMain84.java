0d636a82a3ec8a7a925e2575af36fbd8001cc1c4
Litway_ <xmw0517@gmail.com>
2022-10-19 16:06:17 +0800

    sentinel-openfeign  修改配置 开启支持

--- cloud-alibaba-consumer-order84/src/main/java/com/litway/springcloud/alibaba/SentinelOrderNacosMain84.java
+++ cloud-alibaba-consumer-order84/src/main/java/com/litway/springcloud/alibaba/SentinelOrderNacosMain84.java
@@ -3,9 +3,13 @@ package com.litway.springcloud.alibaba;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+
+// 开启OpenFeign功能
+@EnableFeignClients
 public class SentinelOrderNacosMain84
 {
     public static void main(String[] args) {
