9a9eadb04ec874eda9b61cc9000db79ad41a5606
Litway_ <xmw0517@gmail.com>
2022-10-19 10:51:13 +0800

    sentinel-ribbon  SentinelResource注解没有添加任何fallback 错误会直接返回给前端

--- cloud-alibaba-consumer-order84/src/main/java/com/litway/springcloud/alibaba/controller/ConsumerOrderController.java
+++ cloud-alibaba-consumer-order84/src/main/java/com/litway/springcloud/alibaba/controller/ConsumerOrderController.java
@@ -1,6 +1,7 @@
 package com.litway.springcloud.alibaba.controller;
 
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.atguigu.springcloud.entities.CommonResult;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,8 +23,19 @@ public class ConsumerOrderController {
     private RestTemplate restTemplate;
 
     @GetMapping("/consumer/{id}")
+    @SentinelResource(value = "fallback")
     public CommonResult<?> getPayment(@PathVariable("id") Long userId) {
         CommonResult<?> commonResult = restTemplate.getForObject(SERVICE_URL + "/paymentSQL/" + userId, CommonResult.class, userId);
+
+        if (userId == 4) {
+            throw new IllegalArgumentException("IllegalArgumentException, 非法参数异常....");
+        } else {
+            assert commonResult != null;
+            if (commonResult.getData() == null) {
+                throw new NullPointerException("NullPointerException, 该ID没有对应记录,空指针异常");
+            }
+        }
+
         return new CommonResult<>().success(commonResult);
     }
 
