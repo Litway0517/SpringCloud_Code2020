340f6db442de3c028910e513f36d190132996b62
Litway_ <xmw0517@gmail.com>
2022-10-20 16:52:17 +0800

    stream-rabbit  重复新建一个stream消费者8803

--- cloud-stream-rabbitmq-consumer8803/src/main/java/com/litway/springcloud/StreamConsumerMain8803.java
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/litway/springcloud/StreamConsumerMain8803.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class StreamConsumerMain8002 {
+public class StreamConsumerMain8803 {
 
     public static void main(String[] args) {
-        SpringApplication.run(StreamConsumerMain8002.class, args);
+        SpringApplication.run(StreamConsumerMain8803.class, args);
     }
 
 }
