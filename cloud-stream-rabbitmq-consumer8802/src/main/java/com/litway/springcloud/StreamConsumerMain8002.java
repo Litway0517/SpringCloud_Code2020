443aeca371c11ce52829a65acd5731bcbcab3394
Litway_ <xmw0517@gmail.com>
2022-10-20 16:23:41 +0800

    stream-rabbit  stream消费者模块 controller层需要加上EnableBinding(Sink.class)注解 具体的方法上需要加上StreamListener(Sink.INPUT)注解监听消息频道

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/litway/springcloud/StreamConsumerMain8002.java
@@ -0,0 +1,15 @@
+package com.litway.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class StreamConsumerMain8002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamConsumerMain8002.class, args);
+    }
+
+}
