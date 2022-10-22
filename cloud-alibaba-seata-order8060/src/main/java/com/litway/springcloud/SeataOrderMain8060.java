60039f27f0c567a650be1fb282c98793c5150c6d
Litway_ <xmw0517@gmail.com>
2022-10-22 16:42:48 +0800

    seata  mybatis.mapper-locations这个配置不能写成数组形式 直接跟着后面写就行 主启动类上排除自动注入数据源 因为seata数据源已经配置好了

--- cloud-alibaba-seata-order8060/src/main/java/com/litway/springcloud/SeataOrderMain8060.java
+++ cloud-alibaba-seata-order8060/src/main/java/com/litway/springcloud/SeataOrderMain8060.java
@@ -2,10 +2,12 @@ package com.litway.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+// 取消数据源的自动创建, 使用配置好的seata数据源
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 @EnableFeignClients
 public class SeataOrderMain8060 {
