09fb714d83cf7ee0a2d97753cec2054835d016ff
Litway_ <xmw0517@gmail.com>
2022-10-22 16:41:57 +0800

    seata  配置类 需要搜一下

--- cloud-alibaba-seata-order8060/src/main/java/com/litway/springcloud/controller/OrderController.java
+++ cloud-alibaba-seata-order8060/src/main/java/com/litway/springcloud/controller/OrderController.java
@@ -2,19 +2,30 @@ package com.litway.springcloud.controller;
 
 
 import com.atguigu.springcloud.entities.CommonResult;
+import com.litway.springcloud.domain.Order;
+import com.litway.springcloud.service.OrderService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 
 @RestController
 @RequestMapping("/order")
 public class OrderController {
 
+    @Resource
+    private OrderService orderService;
+
     @GetMapping("/test-api")
     public CommonResult<?> testApi() {
         return new CommonResult<>().success();
     }
 
-
+    @GetMapping("/create")
+    public CommonResult<?> create(Order order) {
+        orderService.create(order);
+        return new CommonResult<>().success("创建成功");
+    }
 }
--- cloud-alibaba-seata-order8060/src/main/java/com/litway/springcloud/service/impl/OrderServiceImpl.java
+++ cloud-alibaba-seata-order8060/src/main/java/com/litway/springcloud/service/impl/OrderServiceImpl.java
@@ -24,7 +24,9 @@ public class OrderServiceImpl implements OrderService {
     private AccountService accountService;
 
     /**
-     * 创建订单
+     * 创建
+     * 创建订单->调用库存服务扣减库存->调用账户服务扣减账户余额->修改订单状态
+     * 下订单->减库存->减余额->改状态
      *
      * @param order 订单
      */
@@ -33,7 +35,13 @@ public class OrderServiceImpl implements OrderService {
         log.info("-----> 开始创建订单");
         orderMapper.create(order);
 
-        log.info("-----> 订单微服务调用库存, 扣减库存");
+        log.info("-----> 订单微服务调用库存, 扣减库存Count");
         storageService.decrease(order.getProductId(), order.getCount());
+
+        log.info("-----> 订单微服务调用库存, 扣减库存Money");
+        accountService.decrease(order.getUserId(), order.getMoney());
+
+        log.info("-----> 修改订单状态");
+        orderMapper.update(order.getUserId(), 0);
     }
 }
