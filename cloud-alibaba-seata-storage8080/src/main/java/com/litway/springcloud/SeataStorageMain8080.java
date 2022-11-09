32010a61700abafb078c5b3afd0848be15e6c532
Litway_ <xmw0517@gmail.com>
2022-11-09 21:53:17 +0800

    seata  SeataFeignClientAutoConfiguration类已经自动注入了FeignAutoConfiguration TraceFeignClientAutoConfiguration类也需要注入一次 因为关闭了重写 所以产生冲突 主启动类上排除掉

--- cloud-alibaba-seata-storage8080/src/main/java/com/litway/springcloud/SeataStorageMain8080.java
+++ cloud-alibaba-seata-storage8080/src/main/java/com/litway/springcloud/SeataStorageMain8080.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.sleuth.instrument.web.client.feign.TraceFeignClientAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, TraceFeignClientAutoConfiguration.class})
 @EnableDiscoveryClient
 @EnableFeignClients
 public class SeataStorageMain8080 {
